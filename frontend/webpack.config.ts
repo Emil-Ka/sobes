af7c4186b19aaff9be2c0d52aed8ebe8d1d10ed6
Emil-Ka <emilkaraev55@gmail.com>
2022-08-16 11:18:50 +0400

    readme добавлен

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { Configuration } from "webpack";
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const config: Configuration = {
   entry: "./src/index.tsx",
@@ -31,17 +31,22 @@ const config: Configuration = {
       },
       {
         test: /\.(sass|scss)$/,
-        use: [MiniCssExtractPlugin.loader, "import-glob-loader", "css-loader", "sass-loader"]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "import-glob-loader",
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
-        use: ["file-loader"]
-      }
+        use: ["file-loader"],
+      },
     ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".jsx"],
-  }
+  },
 };
 
 export default config;
